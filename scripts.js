2f8cb417b4f46bd6b8dc9ff69ec9b30e5586e873
vignesh-inv <vigneshk3663@gmail.com>
2025-12-01 14:25:58 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function appendValue(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let expression = document.getElementById("display").value;
+    try {
+        document.getElementById("display").value = eval(expression);
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
